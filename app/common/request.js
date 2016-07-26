c260150064d55f9606ab76cc54c6e75ce4696415
Scott <banruosheng@gmail.com>
2016-07-26 21:17:05 +0800

    remove mockjs

--- app/common/request.js
+++ app/common/request.js
@@ -2,7 +2,6 @@
 
 var queryString = require('query-string')
 var _ = require('lodash')
-var Mock = require('mockjs')
 var config = require('./config')
 
 var request = {}
@@ -14,7 +13,6 @@ request.get = function(url, params) {
 
   return fetch(url)
     .then((response) => response.json())
-    .then((response) => Mock.mock(response))
 }
 
 request.post = function(url, body) {
@@ -24,7 +22,6 @@ request.post = function(url, body) {
 
   return fetch(url, options)
     .then((response) => response.json())
-    .then((response) => Mock.mock(response))
 }
 
 module.exports = request
